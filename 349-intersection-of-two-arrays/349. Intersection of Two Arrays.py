e133b0abffdcd62885216f4d6ec2e912b905b1e5
Victor T <vitran950@gmail.com>
2024-03-13 08:39:46 -0600

    Runtime: 53 ms (25.18%), Memory: 16.7 MB (91.23%) - Gitcode

---
+++ 349-intersection-of-two-arrays/349. Intersection of Two Arrays.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hash = set(nums1)
+        result = set()
+        for num in nums2:
+            if num in hash:
+                result.add(num)
+        return list(result)

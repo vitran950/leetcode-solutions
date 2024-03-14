9c32598cfdb266b1bd8d7a9d932203ae4032eec4
Victor T <vitran950@gmail.com>
2024-03-14 13:52:10 -0600

    Runtime: 694 ms (33.12%), Memory: 31.2 MB (84.20%) - Gitcode

---
+++ 2270-number-of-ways-to-split-array/2270. Number of Ways to Split Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        prefix = [nums[0]]
+        for i in range(1, n):
+            prefix.append(nums[i] + prefix[-1])
+
+        ans = 0
+        for i in range(n - 1):
+            left_section = prefix[i]
+            right_section = prefix[-1] - prefix[i]
+            if left_section >= right_section:
+                ans += 1
+
+        return ans

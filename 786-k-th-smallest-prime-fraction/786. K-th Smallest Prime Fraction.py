50740f64a317673b006cbbed524df4b1ede40ae1
Victor T <vitran950@gmail.com>
2024-05-14 22:42:38 -0600

    Runtime: 1545 ms (28.64%), Memory: 127.9 MB (7.18%) - Gitcode

--- 786-k-th-smallest-prime-fraction/786. K-th Smallest Prime Fraction.py
+++ 786-k-th-smallest-prime-fraction/786. K-th Smallest Prime Fraction.py
@@ -3,16 +3,14 @@ from collections import defaultdict
 
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        dictionary = defaultdict(list)
         heap = []
+        hash = defaultdict(list)
         for i in range(len(arr)):
             for j in range(i + 1, len(arr)):
                 fraction = arr[i] / arr[j]
-                heap.append(fraction)
-                dictionary[fraction] = [arr[i], arr[j]]
-        heapify(heap)
-        i = 0
-        while i < k - 1:
-            heapq.heappop(heap)
-            i += 1
-        return dictionary[heapq.heappop(heap)]
+                # Use negative fraction because heapq is a min heap
+                heapq.heappush(heap, -fraction)
+                hash[-fraction] = [arr[i], arr[j]]
+                if len(heap) > k:
+                    heapq.heappop(heap)
+        return hash[heapq.heappop(heap)]

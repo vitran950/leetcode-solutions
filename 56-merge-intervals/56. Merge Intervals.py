9c929b9da55a92d0286fe80d107c981a151475c9
Victor Tran <vitran950@gmail.com>
2025-11-15 21:46:15 -0700

    Runtime: 15 ms (9.62%), Memory: 20.8 MB (89.98%) - Gitcode

--- 56-merge-intervals/56. Merge Intervals.py
+++ 56-merge-intervals/56. Merge Intervals.py
@@ -1,14 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        stack = [intervals[0]]
-        
+        ans = [intervals[0]]
         for i in range(1, len(intervals)):
-            curr = intervals[i]
-            if stack[-1][1] >= curr[0]:
-                prev = stack.pop()
-                stack.append([prev[0], max(curr[1], prev[1])])
+            interval = intervals[i]
+            start = interval[0]
+            end = interval[1]
+            if start > ans[-1][1]:
+                ans.append(interval)
             else:
-                stack.append(curr)
-
-        return stack    
+                ans[-1] = [ans[-1][0], max(end, ans[-1][1])]
+        return ans

3b2028182a2c4297c4811c7d6533f8ffbc32b7d7
Victor Tran <vitran950@gmail.com>
2025-11-20 12:02:47 -0700

    Runtime: 46 ms (90.97%), Memory: 22.3 MB (20.00%) - Gitcode

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        
+        if root == q or root == p:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        
+        if left:
+            return left
+        
+        if right:
+            return right
+        
+        return None
